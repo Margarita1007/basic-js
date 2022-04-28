0911eb266162e32e9a4b940d59859dd532d6dfeb
Margarita1007 <m.kenshova@mail.ru>
2022-04-28 23:05:29 +0400

    feat: complete 6 tasks

--- src/what-season.js
+++ src/what-season.js
@@ -13,68 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   
- // if (date == []) { return 'Unable to determine the time of year!'; }
-
- // if (Reflect.ownKeys(date).length > 1) {    throw new Error("Invalid date!");  }
-
- 
-try {
-
- 
- 
- //if ((date instanceof Date) == "false") {    throw new Error("Invalid date!");  }
-  
-switch (date.getMonth()) {
-  case 11:
-  case 0:    
-  case 1:
-    return "winter";
-
-  case 2:
-  case 3:    
-  case 4:
-    return "spring";
-
-
-  case 5:
-  case 6:    
-  case 7:
-    return "summer";  
-
-  case 8:
-  case 9:    
-  case 10:
-    return "autumn";      
+  if (date == undefined) {
+    return "Unable to determine the time of year!";
   }
-  
-  
-}
 
-
-
-
-catch(e) {
-
- 
-  
-  if (e.message.includes("is not a function")) {
+  if (typeof date == 'object' && Reflect.ownKeys(date)[0]) {
     throw new Error("Invalid date!");
   } else {
-    if (e.message.includes("read properties")) {
-      return 'Unable to determine the time of year!';
-  }
-
-
- }  
  
- //if ((typeof (Reflect.ownKeys(date)[0]) == 'string')) {    throw new Error("Invalid date!");  }
- if (Reflect.ownKeys(date).length > 1) {    throw new Error("Invalid date!");  }
- //if (Reflect.ownKeys(date).length > 8) {    throw new Error("Invalid date!");  }
+    try {
 
+      switch (date.getMonth()) {
+  
+          case 11:
+          case 0:
+          case 1:
+            return"winter";
+  
+          case 2:
+          case 3:
+          case 4:
+            return "spring";
+  
+          case 5:
+          case 6:
+          case 7:
+            return "summer";
+  
+          case 8:
+          case 9:
+          case 10:
+            return "fall";
+        }
+      }
+      catch (e) {
+  
+            if (e instanceof TypeError) {
+              throw new Error("Invalid date!");
+            } 
+  
+      }
+    }
+ 
 
-}
-}
-
+  }
+  
 module.exports = {
   getSeason
 };
