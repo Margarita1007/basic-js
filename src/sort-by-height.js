c361db68836ab1cbd6bb2a1b629cf7d86c1c8b20
Margarita1007 <m.kenshova@mail.ru>
2022-05-03 03:37:09 +0400

    feat: + some task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -14,26 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
 function sortByHeight(arr) {
 
   let arr1 = [];
-  let res = new Array(arr.length);
+  let res = [] //= new Array(arr.length);
   /*for (let i = 0; i < res.length; i++) {
     delete res[i];} */
   
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < res.length; j++) {
     if (arr[i]=='-1') {
-     // delete arr[i]
-      res[j]=arr[i]
+     arr1.push(i)  ///// индексы -1
     }
-  }
   }
 
   
   let arrSort = arr.sort();
   
- for (let i = 0; i < res.length; i++) {
-    for (let j = 0; j < arrSort.length; j++) {
-    if (res[i]!=-1) {res[i]=arrSort[j]}
-  } }
+ for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      for (let k = 0; k < arrSort.length; k++) {
+      
+      if (arr1[j]==i) {res.push(-1); break; break} else {res.push(arrSort[k])}
+  } } }
 
   return res;
   
